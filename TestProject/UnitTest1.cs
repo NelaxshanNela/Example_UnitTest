336808939c6e8fb6d287bff7a3792c2d0900ccc4
Nelaxshan UCA <nelaxshan@unicornconnectedapps.com>
2025-03-26 10:34:39 +0530

    example for theroy attribute added

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -17,5 +17,16 @@ namespace TestProject
             // Assert
             Assert.Equal(5, result);
         }
+
+        [Theory]
+        [InlineData(2, 3, 5)]
+        [InlineData(-1, -1, -2)]
+        [InlineData(0, 5, 5)]
+        public void Add_ShouldReturnCorrectSum_WithMultipleInputs(int a, int b, int expected)
+        {
+            var calculator = new Calculator();
+            int result = calculator.Add(a, b);
+            Assert.Equal(expected, result);
+        }
     }
 }
